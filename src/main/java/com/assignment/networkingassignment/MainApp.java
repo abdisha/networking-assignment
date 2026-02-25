16b3fff08fa77c5522a38feacf6fd0c0a0d08d54
Abdi <aag2216@cumc.columbia.edu>
2026-02-25 16:31:40 +0300

    configured for packaging

--- src/main/java/com/assignment/networkingassignment/MainApp.java
+++ src/main/java/com/assignment/networkingassignment/MainApp.java
@@ -15,6 +15,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 
 public class MainApp extends Application implements ServerListener {
     private final ObservableList<String> userList = FXCollections.observableArrayList();
@@ -26,12 +29,16 @@ public class MainApp extends Application implements ServerListener {
         TextField adminInput = new TextField();
         Button broadcastBtn = new Button("Broadcast");
 
+        String myIp = getServerIP();
+        Label ipLabel = new Label("Server IP: " + myIp + " | Port: 65432");
+        ipLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: blue;");
+
         broadcastBtn.setOnAction(e -> {
             server.broadcast("ADMIN: " + adminInput.getText());
             adminInput.clear();
         });
 
-        VBox root = new VBox(10, new Label("Online Users:"), listView, new HBox(5, adminInput, broadcastBtn));
+        VBox root = new VBox(10,ipLabel, new Label("Online Users:"), listView, new HBox(5, adminInput, broadcastBtn));
         root.setPadding(new Insets(15));
 
         server = new TcpEngine(65432, this);
@@ -40,6 +47,19 @@ public class MainApp extends Application implements ServerListener {
         primaryStage.setTitle("TCP Server Monitor");
         primaryStage.setScene(new Scene(root, 400, 500));
         primaryStage.show();
+    }
+
+
+
+    public String getServerIP() {
+        try (final DatagramSocket socket = new DatagramSocket()) {
+            // We "connect" to a dummy address to force the OS to pick
+            // the correct network interface used for internet/LAN.
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            return socket.getLocalAddress().getHostAddress();
+        } catch (Exception e) {
+            return "127.0.0.1"; // Fallback
+        }
     }
 
     @Override public void onUserConnect(String ip) { Platform.runLater(() -> userList.add(ip)); }
